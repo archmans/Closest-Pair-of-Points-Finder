fce6721bd586667f1f57d44fa715c1536b3d1593
archmans <13521010@std.stei.itb.ac.id>
2023-02-26 16:41:54 +0700

    FIX modul

--- divideConquer.py
+++ divideConquer.py
@@ -7,21 +7,24 @@ import time
 import os
 counterEuclidean = 0
 
-def generate_points(n):
+def generate_points(n, m):
   #complexity O(n)
-    points = []
-    for i in range(n):
-        x = random.randint(0,100)
-        y = random.randint(0,100)
-        z = random.randint(0,100)
-        points.append([x,y,z])
-    return points
+  points = []
+  for i in range(n):
+      temp = []
+      for j in range(m):
+          temp.append(random.randint(0,1000))
+      points.append(temp)
+  return points
 
 def distance(p1,p2):
   #complexity O(1)
-    global counterEuclidean
-    counterEuclidean+=1
-    return math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2 + (p1[2]-p2[2])**2)
+  global counterEuclidean
+  counterEuclidean+=1
+  result = float(0)
+  for i in range(len(p1)):
+      result += (p1[i]-p2[i])**2
+  return math.sqrt(result)
 
 def closest_pair(points):
   #complexity O(nlogn)
@@ -49,8 +52,6 @@ def last_step(points):
     mid = int(len(points)//2) + 1
   left = points[:mid]
   right = points[mid:]
-  # print("left", left)
-  # print("right", right)
   p1, q1, d1 = closest_pair(left)
   p2, q2, d2 = closest_pair(right)
   if(d1 < d2):
@@ -61,59 +62,62 @@ def last_step(points):
     d = d2
     p = p2
     q = q2
-  midx = points[mid][0]
-  midy = points[mid][1]
-  midz = points[mid][2]
-  new_list = []
   for i in range(len(points)):
-    if(abs(points[i][0] - midx) < d or abs(points[i][1] - midy) < d or abs(points[i][2] - midz) < d):
-      new_list.append(points[i])
-  for i in range(len(new_list)):
-    for j in range(i+1, len(new_list)):
-      if(distance(new_list[i], new_list[j]) < d):
-        d = distance(new_list[i], new_list[j])
-        p = new_list[i]
-        q = new_list[j]
+    if(abs(points[i][0] - points[mid][0]) < d):
+      for j in range(i+1, len(points)):
+        if(abs(points[j][0] - points[mid][0]) < d):
+          if(distance(points[i], points[j]) < d):
+            d = distance(points[i], points[j])
+            p = points[i]
+            q = points[j]
   return p, q, d  
 
-def visualize_closest_pair(points):
+def visualize_closest_pair3(points):
   #complexity O(nlogn)
-    p, q, d = closest_pair(points)
-    x = []
-    y = []
-    z = []
-    for i in range(len(points)):
-      if(points[i] != p and points[i] != q):
-        x.append(points[i][0])
-        y.append(points[i][1])
-        z.append(points[i][2])
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(x,y,z)
-    ax.scatter(p[0],p[1],p[2], color='r')
-    ax.scatter(q[0],q[1],q[2], color='r')
-    ax.plot([p[0],q[0]],[p[1],q[1]],[p[2],q[2]], color='r')
-    plt.show()
+  p, q, d = closest_pair(points)
+  x = []
+  y = []
+  z = []  
+  for i in range(len(points)):
+    if(points[i] != p and points[i] != q):
+      x.append(points[i][0])
+      y.append(points[i][1])
+      z.append(points[i][2])
+  fig = plt.figure()
+  ax = fig.add_subplot(111, projection='3d')
+  ax.scatter(x,y,z)
+  ax.scatter(p[0],p[1],p[2], color='r')
+  ax.scatter(q[0],q[1],q[2], color='r')
+  ax.plot([p[0],q[0]],[p[1],q[1]],[p[2],q[2]], color='r')
+  plt.show()
 
-def visualize_closest_pair_bf(points):
-  #complexity O(n^2)
-    p1, q1, d1 = closest_pair_bf(points)
-    last_step(points, p1, q1, d1)
-    x = []
-    y = []
-    z = []
-    for i in range(len(points)):
-      if(points[i] != p and points[i] != q):
-        x.append(points[i][0])
-        y.append(points[i][1])
-        z.append(points[i][2])
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(x,y,z).set_color('blue')
-    ax.scatter(p[0],p[1],p[2]).set_color('red')
-    ax.scatter(q[0],q[1],q[2]).set_color('red')
-    ax.plot([p[0],q[0]],[p[1],q[1]],[p[2],q[2]]).set_color('red')  
-    plt.show()
+def visualize_closest_pair2(points):
+  p, q, d = closest_pair(points)
+  x = []
+  y = []
+  for i in range(len(points)):
+      if (points[i] != p and points[i] != q):
+          x.append(points[i][0])
+          y.append(points[i][1])
+  fig = plt.figure()
+  plt.scatter(x,y, c='b', )
+  plt.scatter(p[0],p[1], color='r')
+  plt.scatter(q[0],q[1], color='r')
+  plt.plot([p[0],q[0]],[p[1],q[1]], color='r')
+  plt.show()
+
+def visualize_closest_pair1(points):
+  p, q, d = closest_pair(points)
+  x = []
+  for i in range(len(points)):
+      if (points[i] != p and points[i] != q):
+          x.append(points[i][0])
+  fig = plt.figure()
+  plt.scatter(x,[0]*len(x), c='b')
+  plt.scatter(p[0],0, color='r')
+  plt.scatter(q[0],0, color='r')
+  plt.plot([p[0],q[0]],[0,0], color='r')
+  plt.show()
 
 def closest_pair_bf(points):
   #complexity O(n^2)
@@ -130,10 +134,12 @@ def closest_pair_bf(points):
   return p, q, d
 
 n = int(input("Masukkan jumlah titik: "))
+m = int(input("Masukkan jumlah dimensi: "))
 while(n < 2):
     n = int(input("Masukkan jumlah titik: "))
+    m = int(input("Masukkan jumlah dimensi: "))
 
-points = generate_points(n)    
+points = generate_points(n, m)    
 os.system('cls' if os.name == 'nt' else 'clear')
 print("============================================================")
 print("                 Divide & Conquer Algorithm                 ")
@@ -156,12 +162,19 @@ endTime2 = time.time()
 totalTime2 = (endTime2 - startTime2)*1000
 print("Waktu eksekusi brute force: ", totalTime2, "ms")
 print("Jumlah operasi euclidean brute force: ", counterEuclidean)
-print("Platfprm: ", platform.processor())
+print("Platform: ", platform.processor())
 print("Jarak terdekat: ", d1)
 print("============================================================")
 print("Apakah anda yakin ingin melihat visualisasi? (yay/nay)")
 answer = input()
 if(answer == 'yay'):
-  visualize_closest_pair(points)
+    if m == 1:
+        visualize_closest_pair1(points)
+    elif m == 2:
+        visualize_closest_pair2(points)
+    elif m == 3:
+        visualize_closest_pair3(points)
+    else:
+        print("Maaf, visualisasi hanya tersedia untuk 1, 2, dan 3 dimensi")
 else:
-  print("Thank you for using our program!")
+  print("Terima kasih telah menggunakan program kami!")
